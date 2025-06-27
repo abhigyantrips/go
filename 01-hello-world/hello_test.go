c8386a08f86cb73f1dd1829ba83be96d7af9710f
Abhigyan Trips <contact@abhigyantrips.dev>
2025-06-27 15:49:17 +0530

    feat(learning): start all over again

---
+++ 01-hello-world/hello_test.go
@@ -0,0 +1,24 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	t.Run("saying hello to myself", func (t *testing.T) {
+		got := Hello("Abhigyan")
+		want := "Hello, Abhigyan"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("saying hello with empty string", func (t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
--- 02-variables-1/03-const/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	const hoursInDay = 24
-	// hoursInDay = 42
-	fmt.Println("There are", hoursInDay, "hours in a day")
-}
